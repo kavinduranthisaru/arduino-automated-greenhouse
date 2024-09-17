872dbcecad8d1f2f8d40f4b4c93ff9fbedb6b4b4
kavinduranthisaru <kavinduranthisaru23@gmail.com>
2024-09-17 17:29:33 +0530

    Fix

--- sketch_sep16c/sketch_sep16c.ino
+++ sketch_sep16c/sketch_sep16c.ino
@@ -1,5 +1,19 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "TMPL69r-yXF-O"
+#define BLYNK_TEMPLATE_NAME "Group P11 1"
+#define BLYNK_AUTH_TOKEN "gl4oQmenFMzMvro-Bp-DCjDFM65azQYX"
+
+#define WIFI_SSID "Im Not A Witch Im Your Wifi ;)"
+#define WIFI_PASSWORD "Walura@458699%"
+#define WIFI_TIMEOUT_MS 20000
 #include <Arduino.h>
 #include <DHT.h>
+#include <Wire.h>
+#include <SPI.h>
+#include "WiFi.h"
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
 
 #define SM_PIN 4 // D4 PIN
 #define LDR_PIN 2 // D2 PIN
@@ -8,8 +22,38 @@
 
 
 DHT dhtSensor(DHT_PIN,DHT11);
+
+void connectToWiFi()
+{
+  Serial.print("Connecting to WiFi");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  unsigned long startAttemptTime = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS)
+  {
+    Serial.print(".");
+    delay(100);
+    if (WiFi.status() != WL_CONNECTED)
+    {
+      Serial.println(" Failed!");
+      // take action
+    }
+    else
+    {
+      Serial.print("Connected!");
+      Serial.println(WiFi.localIP());
+    }
+  }
+}
+
+
+
+
 void setup() {
-  Serial.begin(115200);
+  connectToWiFi();
+  Serial.begin(9600);
+   Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASSWORD, "blynk.cloud", 80);
+
   dhtSensor.begin();
 
 }
