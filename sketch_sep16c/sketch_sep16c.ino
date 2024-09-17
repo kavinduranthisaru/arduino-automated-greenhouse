a8d60be6e9fdd8f629762ef0dd0acd0333191f9a
kavinduranthisaru <kavinduranthisaru23@gmail.com>
2024-09-17 18:58:11 +0530

    Fix

--- sketch_sep16c/sketch_sep16c.ino
+++ sketch_sep16c/sketch_sep16c.ino
@@ -1,7 +1,7 @@
 #define BLYNK_PRINT Serial
 #define BLYNK_TEMPLATE_ID "TMPL69r-yXF-O"
 #define BLYNK_TEMPLATE_NAME "Group P11 1"
-#define BLYNK_AUTH_TOKEN "gl4oQmenFMzMvro-Bp-DCjDFM65azQYX"
+#define BLYNK_AUTH_TOKEN "CJTZHTAItg-x9yoAH99yg45YIEHhuauW"
 
 #define WIFI_SSID "Im Not A Witch Im Your Wifi ;)"
 #define WIFI_PASSWORD "Walura@458699%"
@@ -13,48 +13,41 @@
 #include "WiFi.h"
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
-
+#include <LiquidCrystal_I2C.h>
 
 #define SM_PIN 4 // D4 PIN
 #define LDR_PIN 2 // D2 PIN
 #define DHT_PIN 5 // D5 PIN
 
-
-
 DHT dhtSensor(DHT_PIN,DHT11);
+LiquidCrystal_I2C lcd(0x01, 16, 2); // SDA -TXD SCL- D22
 
-void connectToWiFi()
-{
-  Serial.print("Connecting to WiFi");
+void connectToWiFi() {
+  Serial.println("Connecting to WiFi...");
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   unsigned long startAttemptTime = millis();
-  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS)
-  {
-    Serial.print(".");
-    delay(100);
-    if (WiFi.status() != WL_CONNECTED)
-    {
-      Serial.println(" Failed!");
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS) {
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.print(" Failed!");
       // take action
-    }
-    else
-    {
-      Serial.print("Connected!");
+    } else {
+      Serial.println("Connected!");
+      Serial.print("Current IP: ");
       Serial.println(WiFi.localIP());
     }
   }
 }
 
-
-
-
 void setup() {
+  Serial.begin(115200);
   connectToWiFi();
-  Serial.begin(9600);
-   Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASSWORD, "blynk.cloud", 80);
-
+  Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASSWORD, "blynk.cloud", 80);
   dhtSensor.begin();
+  
+  // Start I2C communication
+  lcd.init();
+  lcd.noBacklight();
 
 }
 
@@ -66,18 +59,32 @@ void loop() {
   
   
   float smPercentage = ((4095 - smValue) / 4095.0) * 100.0;
+  float ldrPercentage = ((1900 - lightLevel) / 1900.0) * 100.0;
   
-  Serial.print("soil moisture value : ");
+  Blynk.virtualWrite(V0, temperature);
+  Blynk.virtualWrite(V1, humidity);
+  Blynk.virtualWrite(V2, ldrPercentage);
+  Blynk.virtualWrite(V3, smPercentage);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temperature);
+
+  Serial.print("Soil moisture value: ");
   Serial.print(smPercentage);
   Serial.println("%");
-  Serial.print("LDR value : ");
-  Serial.println(lightLevel);
-  Serial.print("temperature value : ");
+  Serial.print("LDR percentage: ");
+  Serial.print(ldrPercentage);
+  Serial.println("%");
+  Serial.print("Temperature value: ");
   Serial.print(temperature);
-  Serial.print(" \t humidity value : ");
-  Serial.println(humidity);
+  Serial.print("°C");
+  Serial.print(" \t Humidity value: ");
+  Serial.print(humidity);
+  Serial.println("%");
 
-  
   delay(2000);
 
 }
+
