4d765eb6d5a3e64e3fa06faa6c1d540d96fa4c6a
kavinduranthisaru <kavinduranthisaru23@gmail.com>
2024-09-21 21:50:03 +0530

    Fix

--- greenhouse/greenhouse.ino
+++ greenhouse/greenhouse.ino
@@ -3,7 +3,7 @@
 #define BLYNK_PRINT Serial
 #define BLYNK_TEMPLATE_ID "TMPL69r-yXF-O"
 #define BLYNK_TEMPLATE_NAME "Group P11 1"
-#define BLYNK_AUTH_TOKEN "R1pTeFHT6IunKTTPmXDZw7QYzdRW4K52"
+#define BLYNK_AUTH_TOKEN "atVBx-PFCYq1ifT__EuDDuPuCQgDxdD5"
 
 #define WIFI_SSID "Im Not A Witch Im Your Wifi ;)"
 #define WIFI_PASSWORD "Walura@458699%"
@@ -28,16 +28,16 @@
 #define DAY_LIGHT_PIN 17 // G17 PIN
 #define WATER_PUMP_PIN 16 // G16 PIN
 
-int TEMPERATURE_UPPER = 32;
-int TEMPERATURE_LOWER = 29;
-int TEMPERATURE_LIMIT = 31;
-int LDR_LIMIT = 36;
-int SM_LIMIT = 50;
-
-// DELAY INSIDE THE LOOP
-int LOOP_DELAY = 2000;
+int TEMP_UPPER = 32;
+int TEMP_LOWER = 28;
+int LDR_THRESHOLD = 36;
+int SM_THRESHOLD = 50;
 
 DHT dhtSensor(DHT_PIN,DHT11);
+BlynkTimer timer;
+
+int DATA_INTERVAL = 2000L;
+int WiFi_INTERVAL = 30000L;
 
 void connectToWiFi() {
   Serial.println("Connecting to WiFi...");
@@ -48,10 +48,10 @@ void connectToWiFi() {
 
   while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS) {
     if (WiFi.status() != WL_CONNECTED) {
-      Serial.println(" Failed!");
+      Serial.println("Connection Failed!");
 
     } else {
-      Serial.println("Connected!");
+      Serial.println("WiFi Connected!");
       Serial.print("Current IP: ");
       Serial.println(WiFi.localIP());
 
@@ -59,67 +59,36 @@ void connectToWiFi() {
   }
 }
 
-// void WiFiStatus() {
-//    if (WiFi.status() != WL_CONNECTED) {
-//     Serial.println("Not Connected!");
+void WiFiStatus() {
+  if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("WiFi Status: Not Connected! \n");
 
-//    } else {
-//     Serial.println("Connected!");
-
-//    }
-// }
-
-// void temperatureHandler() {
-//   if (temperature > TEMPERATURE_UPPER) {
-//     digitalWrite(HEATER_PIN, HIGH);
-
-//   } else if (temperature == TEMPERATURE_LIMIT) {
-//     digitalWrite(HEATER_PIN, LOW);
-
-//   }
-
-//   if (temperature < TEMPERATURE_LOWER) {
-//     digitalWrite(COOLER_PIN, HIGH);
-
-//   } else if (temperature == TEMPERATURE_LIMIT) {
-//     digitalWrite(COOLER_PIN, LOW);
-
-//   }
-//  }
-
-//  void sunlightHandler() {
-//   if (ldrPercentage < LDR_LIMIT) {
-//     digitalWrite(DAY_LIGHT_PIN, HIGH);
-
-//   } else if (ldrPercentage > LDR_LIMIT) {
-//     digitalWrite(DAY_LIGHT_PIN, LOW);
-
-//   }
-//  }
-
-//  void soilmoistureHandler() {
-//   if (smPercentage < SM_LIMIT) {
-//     digitalWrite(WATER_PUMP_PIN, HIGH);
-
-//   } else if (smPercentage > SM_LIMIT) {
-//     digitalWrite(WATER_PUMP_PIN, LOW);
-
-//   }
-//  }
-
-void setup() {
-  Serial.begin(115200);
-  connectToWiFi();
-  Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASSWORD, "blynk.cloud", 80);
-  dhtSensor.begin();
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(16, OUTPUT);
-  pinMode(17, OUTPUT);
-
+    } else {
+      Serial.println("WiFi Status: Connected! \n");
+    }
 }
 
-void loop() {
+BLYNK_CONNECTED() {
+  Blynk.syncVirtual(V4, V5, V6, V7);
+}
+
+BLYNK_WRITE(V4) {
+  TEMP_UPPER = param.asInt();
+}
+
+BLYNK_WRITE(V5) {
+  TEMP_LOWER = param.asInt();
+}
+
+BLYNK_WRITE(V6) {
+  LDR_THRESHOLD = param.asInt();
+}
+
+BLYNK_WRITE(V7) {
+  SM_THRESHOLD = param.asInt();
+}
+
+void sendData() {
   int smValue = analogRead(SM_PIN);
   int ldrValue = analogRead(LDR_PIN);
   float temperature = dhtSensor.readTemperature();
@@ -128,24 +97,47 @@ void loop() {
   float smPercentage = ((4095 - smValue) / 4095.0) * 100.0;
   float ldrPercentage = ((4095 - ldrValue) / 4095.0) * 100.0;
 
-  // WiFiStatus();
-  // temperatureHandler();
-  // sunlightHandler();
-  // soilmoistureHandler();
+  int TEMP_THRESHOLD = (TEMP_UPPER + TEMP_LOWER) / 2;
 
-  if (temperature > TEMPERATURE_UPPER) {
-    digitalWrite(COOLER_PIN, HIGH);
-
-  } else if (temperature < TEMPERATURE_LIMIT) {
+  // COOLER HANDLER
+  if (temperature > TEMP_UPPER) {
     digitalWrite(COOLER_PIN, LOW);
+    Blynk.virtualWrite(V12, 1);
+  } else if (temperature <= TEMP_THRESHOLD) {
+    digitalWrite(COOLER_PIN, HIGH);
+    Blynk.virtualWrite(V12, 0);
+  }
+
+  // HEATER HANDLER
+  if (temperature < TEMP_LOWER) {
+    digitalWrite(HEATER_PIN, LOW);
+    Blynk.virtualWrite(V13, 1);
+
+  } else if (temperature >= TEMP_THRESHOLD) {
+    digitalWrite(HEATER_PIN, HIGH);
+    Blynk.virtualWrite(V13, 0);
 
   }
 
-  if (temperature < TEMPERATURE_LOWER) {
-    digitalWrite(HEATER_PIN, HIGH);
+  // DAY LIGHT HANDLER
+  if (ldrPercentage < LDR_THRESHOLD) {
+    digitalWrite(DAY_LIGHT_PIN, LOW);
+    Blynk.virtualWrite(V14, 1);
 
-  } else if (temperature > TEMPERATURE_LIMIT) {
-    digitalWrite(HEATER_PIN, LOW);
+  } else {
+    digitalWrite(DAY_LIGHT_PIN, HIGH);
+    Blynk.virtualWrite(V14, 0);
+
+  }
+
+  // WATER PUMP HANDLER
+  if (smPercentage < SM_THRESHOLD) {
+    digitalWrite(WATER_PUMP_PIN, LOW);
+    Blynk.virtualWrite(V15, 1);
+
+  } else {
+    digitalWrite(WATER_PUMP_PIN, HIGH);
+    Blynk.virtualWrite(V15, 0);
 
   }
   
@@ -165,45 +157,36 @@ void loop() {
   Serial.print("°C");
   Serial.print(" \t Humidity value: ");
   Serial.print(humidity);
-  Serial.println("%");
-
-  delay(LOOP_DELAY);
+  Serial.println("% \n");
 
 }
 
+void setup() {
+  Serial.begin(115200);
+  dhtSensor.begin();
+  connectToWiFi();
+  Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASSWORD, "blynk.cloud", 80);
+  timer.setInterval(DATA_INTERVAL, sendData);
+  timer.setInterval(WiFi_INTERVAL, WiFiStatus);
 
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(16, OUTPUT);
+  pinMode(17, OUTPUT);
 
-// // LCD ADDRESS SCANNER
-// #include <Wire.h>
+  Blynk.virtualWrite(V4, TEMP_UPPER);
+  Blynk.virtualWrite(V5, TEMP_LOWER);
+  Blynk.virtualWrite(V6, LDR_THRESHOLD);
+  Blynk.virtualWrite(V7, SM_THRESHOLD);
 
-// void setup() {
-//   Serial.begin (9600);
-//   while (!Serial) 
-//     {
-//     }
+  digitalWrite(COOLER_PIN, LOW);
+  digitalWrite(HEATER_PIN, LOW);
 
-//   Serial.println ();
-//   Serial.println ("HRTE PROJECTS I2C address. Scanning ...");
-//   byte count = 0;
+}
+
+void loop() {
+  Blynk.run();
+  timer.run();
   
-//   Wire.begin();
-//   for (byte i = 8; i < 120; i++)
-//   {
-//     Wire.beginTransmission (i);
-//     if (Wire.endTransmission () == 0)
-//       {
-//       Serial.print ("Found address: ");
-//       Serial.print (i, DEC);
-//       Serial.print (" (0x");
-//       Serial.print (i, HEX);
-//       Serial.println (")");
-//       count++; 
-//   } 
-//   }
-//   Serial.println ("Done.");
-//   Serial.print ("Found ");
-//   Serial.print (count, DEC);
-//   Serial.println (" device(s).");
-// }  
+}
 
-// void loop() {}
